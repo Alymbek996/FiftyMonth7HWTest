8f006b23b1258635da4147b6737ba3c57a223d2f
kyial4 <ik.dreamer7@gmail.com>
2022-08-14 12:19:56 +0600

    android test12

--- app/src/androidTest/java/com/geektech/fiftymonth7hwtest44/MainActivityTest.kt
+++ app/src/androidTest/java/com/geektech/fiftymonth7hwtest44/MainActivityTest.kt
@@ -2,6 +2,7 @@ package com.geektech.fiftymonth7hwtest44
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
